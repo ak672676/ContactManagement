cf8d4302d32f9255624a03bebe6235feac3d53cb
ak672676 <ak672676@gmail.com>
2019-01-22 00:14:22 +0530

    Updates

---
+++ precache-manifest.df7314279b3b4b7f44f08f232b53e5c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac5dec8a75f1158ed082",
+    "url": "/ContactManagement/static/js/runtime~main.ac5dec8a.js"
+  },
+  {
+    "revision": "76ca46ac62e9ca2f0dc2",
+    "url": "/ContactManagement/static/js/main.76ca46ac.chunk.js"
+  },
+  {
+    "revision": "285b51ca62912fe9d148",
+    "url": "/ContactManagement/static/js/1.285b51ca.chunk.js"
+  },
+  {
+    "revision": "76ca46ac62e9ca2f0dc2",
+    "url": "/ContactManagement/static/css/main.f6e80481.chunk.css"
+  },
+  {
+    "revision": "285b51ca62912fe9d148",
+    "url": "/ContactManagement/static/css/1.6ff159e9.chunk.css"
+  },
+  {
+    "revision": "7d41436b6e1b7201974c58ab39729153",
+    "url": "/ContactManagement/index.html"
+  }
+];
